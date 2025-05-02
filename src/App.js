95406c10e8de70e6adeb62163ef1a834d0009d93
Muhamad Yusuf Al Farizzi <muhammadyusufalfarizzi@gmail.com>
2025-05-02 22:09:54 +0700

    Initial Kontak

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route } from 'react-router-dom';
 import Beranda from './pages/Beranda/Beranda';
 import SaranKeluhan from './pages/sarankeluhan/SaranKeluhan';
 import TentangPuskesmas from './pages/TentangPuskesmas/TentangPuskesmas';
+import Kontak from './pages/Kontak/Kontak';
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <Route path="/" element={<Beranda />} />
         <Route path="/tentangPuskesmas" element={<TentangPuskesmas />} />
         <Route path="/saranKeluhan" element={<SaranKeluhan />} />
-        <Route path="/kontak" element={<h1>Kontak</h1>} />
+        <Route path="/kontak" element={<Kontak />} />
         <Route path="/layanan/:id" element={<h1>Layanan Detail</h1>} />
       </Routes>
       <Footer />
---
+++ src/pages/Kontak/Kontak.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Kontak.css';
+
+const Kontak = () => {
+  return (
+    <div>
+      <div className="banner">
+        <h1>KONTAK KAMI</h1>
+        <p>Anda dapat menghubungi kami melalui kontak di bawah ini.</p>
+      </div>
+
+      <div className="content">
+      </div>
+    </div>
+  );
+};
+
+export default Kontak;
