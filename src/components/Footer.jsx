60509f606f34036b5aaede8e7a6aefb00f811e74
Muhamad Yusuf Al Farizzi <muhammadyusufalfarizzi@gmail.com>
2025-05-03 10:27:21 +0700

    Kontak

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,25 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h3>Puskesmas Kujangsari</h3>
-        <ul>
-          <li><i className="fab fa-whatsapp"></i> +6282129690500</li>
-          <li><i className="fas fa-phone-alt"></i> (022) 7530465</li>
-          <li><i className="fab fa-instagram"></i> @uptd_puskesmaskujangsari</li>
-          <li><i className="fas fa-envelope"></i> uptdpkmkujangsari@gmail.com</li>
-        </ul>
+        <div className="footer-left">
+          <h3>Puskesmas Kujangsari</h3>
+          <ul>
+            <a href="https://wa.me/6282129690500" target="_blank" rel="noopener noreferrer">
+              <li><i className="fab fa-whatsapp"></i> +6282129690500</li>
+            </a>
+            <a href="tel:0227530456">
+              <li><i className="fas fa-phone-alt"></i> (022) 7530465</li>
+            </a>
+            <a href="https://instagram.com/uptd_puskesmaskujangsari" target="_blank" rel="noopener noreferrer">
+              <li><i className="fab fa-instagram"></i> @uptd_puskesmaskujangsari</li>
+            </a>
+            <li><i className="fas fa-envelope"></i> uptdpkmkujangsari@gmail.com</li>
+          </ul>
+        </div>
+
+        <div className="footer-right">
+          <p>&copy; 2025 Muhamad Yusuf Al Farizzi</p>
+        </div>
       </div>
     </footer>
   );
--- src/pages/Kontak/Kontak.jsx
+++ src/pages/Kontak/Kontak.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Kontak.css';
+import { FaWhatsapp, FaInstagram, FaPhoneAlt } from 'react-icons/fa';
 
 const Kontak = () => {
   return (
@@ -8,9 +9,44 @@ const Kontak = () => {
         <h1>KONTAK KAMI</h1>
         <p>Anda dapat menghubungi kami melalui kontak di bawah ini.</p>
       </div>
-
+      
       <div className="content">
+
+      <a href="https://wa.me/6282129690500" target="_blank" rel="noopener noreferrer">
+        <div className="contact-box">
+          <FaWhatsapp size={92} />
+          <p>+62 82129690500</p>
+        </div>
+      </a>
+
+      <a href="https://instagram.com/uptd_puskesmaskujangsari" target="_blank" rel="noopener noreferrer">
+        <div className="contact-box">
+          <FaInstagram size={92}/>
+          <p>@uptd_puskesmaskujangsari</p>
+        </div>
+      </a>
+
+      <a href="tel:0227530456">
+        <div className="contact-box">
+          <FaPhoneAlt size={92}/>
+          <p>(022) 7530456</p>
+        </div>
+      </a>
       </div>
+
+      <h2>Lokasi Puskesmas Kujangsari</h2>
+      <div className="maps-container">
+        <div className="map-responsive">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.3767112115115!2d107.63529177500814!3d-6.964809868193736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e84c699369eb%3A0xefed71cd08faafb!2sPuskesmas%20Kujangsari!5e0!3m2!1sen!2sid!4v1746241511501!5m2!1sen!2sid"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Peta Puskesmas Kujangsari"
+          ></iframe>
+        </div>
+      </div>
+
     </div>
   );
 };
